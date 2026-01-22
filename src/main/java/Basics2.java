8bf38f9d69f9f26e5294e08db7630eaad4d79399
Bharath <bharathboya60@gmail.com>
2026-01-22 18:42:50 +0530

    Pushed the javaProject

---
+++ src/main/java/Basics2.java
@@ -0,0 +1,16 @@
+
+public class Basics2 {
+
+	public static void main(String[] args) {
+		int Number = 15;
+		String Name = "Bharath";
+		boolean IsJavaFun = true;
+		char Grade = 'A';
+		double DecimalNumber = 10.5;
+		
+		System.out.println("Integer: " + Number);
+		
+
+	}
+
+}
